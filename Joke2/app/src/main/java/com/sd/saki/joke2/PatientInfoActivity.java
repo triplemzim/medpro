c6ca9113e93a373b6a1b8615283a11f6dd8f7635
Shahriar Kabir <a.toxophilite@gmail.com>
2016-06-28 01:30:39 +0600

    tried grid layout

--- Joke2/app/src/main/java/com/sd/saki/joke2/PatientInfoActivity.java
+++ Joke2/app/src/main/java/com/sd/saki/joke2/PatientInfoActivity.java
@@ -10,6 +10,6 @@ public class PatientInfoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patientinfo);
+        setContentView(R.layout.testgridlayout);
     }
 }
